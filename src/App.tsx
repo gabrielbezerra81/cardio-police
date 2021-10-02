14e5fa62163e8cc02a3e549cc9422d87854f566a
Gabriel Alencar <gabriel_alencar2@hotmail.com>
2021-10-02 20:44:34 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "./assets/icon.png";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <img src={logo} alt="Escola Paulista de Medicina" />
+
+        <h1>Política de Privacidade</h1>
+
+        <span></span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
