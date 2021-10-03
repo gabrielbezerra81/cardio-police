af1c09751a39d47cafa92f1facf1d88fc2f164f3
Gabriel Alencar <gabriel_alencar2@hotmail.com>
2021-10-02 21:04:31 -0300

    adicionando textos

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,100 @@ function App() {
 
         <h1>Política de Privacidade</h1>
 
-        <span></span>
+        <p>
+          A presente Política de Privacidade contém informações sobre coleta,
+          uso, armazenamento, tratamento e proteção dos dados pessoais dos
+          usuários e visitantes do aplicativo CARDIO SCAP APP, com a finalidade
+          de demonstrar absoluta transparência quanto ao assunto e esclarecer a
+          todos interessados sobre os tipos de dados que são coletados, os
+          motivos da coleta e a forma como os usuários podem gerenciar ou
+          excluir as suas informações pessoais. O presente documento foi
+          elaborado em conformidade com a Lei Geral de Proteção de Dados
+          Pessoais (Lei 13.709/18), o Marco Civil da Internet (Lei 12.965/14) (e
+          o Regulamento da UE n. 2016/6790). Ainda, o documento poderá ser
+          atualizado em decorrência de eventual atualização normativa, razão
+          pela qual se convida o usuário a consultar periodicamente esta seção.
+        </p>
+
+        <br />
+        <strong>1. Quem deve utilizar nosso aplicativo</strong>
+        <p>
+          Nosso aplicativo só deve ser utilizado por pessoas com mais de dezoito
+          anos de idade. Sendo assim, crianças e adolescentes não podem
+          utilizá-lo.
+        </p>
+
+        <br />
+        <strong>2. Dados que coletamos e motivo da coleta</strong>
+        <p>
+          Nosso aplicativo coleta e utiliza somente dados fornecidos
+          explicitamente pelos usuários, de acordo com o disposto nesta seção.
+          <p>A. Dados sensíveis</p>
+          <strong>Não</strong> serão coletados dados sensíveis de nossos
+          usuários, assim entendidos aqueles definidos nos arts. 11 e seguintes
+          da Lei de Proteção de Dados. Assim, não haverá coleta de dados de
+          origem racial ou étnica, convicção religiosa, opinião política,
+          filiação a sindicato ou a organização de caráter religioso, filosófico
+          ou político, referente à saúde ou à vida sexual, dado genético ou
+          biométrico, quando vinculado a uma pessoa natural.
+          <p>B. Coleta de dados não previstos expressamente</p>
+          Eventualmente, outros tipos de dados não previstos expressamente nesta
+          Política de Privacidade poderão ser coletados, desde que sejam
+          fornecidos com o consentimento do usuário, ou, ainda, que a coleta
+          seja permitida com fundamento em outra base legal prevista em lei. Em
+          qualquer caso, a coleta de dados e as atividades de tratamento dela
+          decorrentes serão informadas aos usuários do aplicativo.
+        </p>
+
+        <br />
+        <strong> 3. Compartilhamento de dados pessoais com terceiros</strong>
+        <p>
+          Não compartilhamos seus dados com terceiros. Apesar disso, é possível
+          que o façamos para cumprir alguma determinação legal ou regulatória,
+          ou, ainda, para cumprir alguma ordem expedida por autoridade pública.
+        </p>
+
+        <br />
+        <strong>4. Segurança e armazenamento de dados pessoais</strong>
+        <p>
+          <strong>Não</strong> armazenamos dados pessoais dos usuários. As
+          informações inseridas explicitamente pelos usuários no aplicativo são
+          utilizadas apenas para o cálculo de simulações, em seguida, após a
+          apresentação dos resultados calculados, todos os dados fornecidos pelo
+          usuário são descartados.
+        </p>
+
+        <br />
+        <strong>5. Consentimento</strong>
+        <p>
+          Ao utilizar nossos serviços, o usuário está consentindo com a presente
+          Política de Privacidade.
+        </p>
+
+        <br />
+        <strong>6. Alterações nesta política</strong>
+        <p>
+          A presente política foi atualizada pela última vez em: 01/10/2021.
+          Reservamos o direito de modificar as normas desta Política de
+          Privacidade a qualquer momento, especialmente para adaptá-las as
+          eventuais alterações feitas em nosso aplicativo, seja pela
+          disponibilização de novas funcionalidades, seja pela supressão ou
+          modificação daquelas já existentes. Sempre que houver uma modificação,
+          nossos usuários serão notificados acerca da mudança.
+        </p>
+
+        <br />
+        <strong>7. Jurisdição</strong>
+        <p>
+          para resolução de conflitos Para a solução de controvérsias
+          decorrentes do presente instrumento será aplicado integralmente o
+          Direito brasileiro. Os eventuais litígios deverão ser apresentados no
+          foro da comarca de Teresina.
+        </p>
+
+        <p>
+          Qualquer dúvida entrar em contato no email: cardioscaapp@gmail.com
+        </p>
       </div>
     </div>
   );
